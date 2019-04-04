737d96e507f4499dbc89bb52ed862c6862c016e1
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2019-04-04 12:22:59 -0600

    Detalles al mostrar archivos en auditoria calidad

--- src/app/calidad-auditoria/calidad-auditoria.component.ts
+++ src/app/calidad-auditoria/calidad-auditoria.component.ts
@@ -658,6 +658,34 @@ export class CalidadAuditoriaComponent implements OnInit, AfterViewChecked, Afte
     downloadLink.click();
   }
 
+  openPdfInTab(archivo) {
+    const base64ImageData = archivo;
+    // let extension = this.base64MimeType(archivo);
+    // console.log(extension);
+    // data:application/pdf
+    const contentType = `application/pdf`;
+
+    const byteCharacters = atob(base64ImageData.substr(`data:${contentType};base64,`.length));
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += 1024) {
+      const slice = byteCharacters.slice(offset, offset + 1024);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      const byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+    }
+    const blob = new Blob(byteArrays, {type: contentType});
+    const blobUrl = URL.createObjectURL(blob);
+
+    window.open(blobUrl, '_blank');
+  }
+
   imprimirDetalle(auditoria) {
     console.log(auditoria);
     this._reporteService.getReporte(auditoria.IdAuditoria, 'Calidad')
--- src/app/calidad-consulta-auditoria/calidad-consulta-auditoria.component.ts
+++ src/app/calidad-consulta-auditoria/calidad-consulta-auditoria.component.ts
@@ -638,6 +638,34 @@ export class CalidadConsultaAuditoriaComponent implements OnInit, OnDestroy, Aft
     downloadLink.click();
   }
 
+  openPdfInTab(archivo) {
+    const base64ImageData = archivo;
+    // let extension = this.base64MimeType(archivo);
+    // console.log(extension);
+    // data:application/pdf
+    const contentType = `application/pdf`;
+
+    const byteCharacters = atob(base64ImageData.substr(`data:${contentType};base64,`.length));
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += 1024) {
+      const slice = byteCharacters.slice(offset, offset + 1024);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      const byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+    }
+    const blob = new Blob(byteArrays, {type: contentType});
+    const blobUrl = URL.createObjectURL(blob);
+
+    window.open(blobUrl, '_blank');
+  }
+
   base64MimeType(encoded) {
     let result = null;
 
